f44e3d4d565b95e751b083ad33848b4393c880a4
Haoran Feng <harrifeng@gmail.com>
2019-08-18 12:44:16 +0000

    seems ok, but no css

--- deploy/ansible/run.sh
+++ deploy/ansible/run.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-ansible-playbook -i ./etc_ansible_hosts main.yml  --extra-vars "variable_host=localhost default_user=${USER}"
+project_dir=$(cd `dirname $0` && cd ..&& cd .. && pwd)
+ansible-playbook -i ./etc_ansible_hosts main.yml  --extra-vars "variable_host=localhost default_user=${USER} django_demo_dir=${project_dir}"
